6440b6b343a6772977822e69d20b664a805d5560
Aleksey710 <mira710@rambler.ru>
2026-03-27 14:52:51 +0200

    wifi custom

--- esp32imu/components/config/include/config_wifi.h
+++ esp32imu/components/config/include/config_wifi.h
@@ -2,10 +2,21 @@
 #define CONFIG_WIFI_H
 //----------------------------------------------------------------------
 #include <inttypes.h>
+#include "config_wifi_custom.h"
 //----------------------------------------------------------------------
 // WiFi
-#define WIFI_SSID ("Afina")
-#define WIFI_PASSWORD ("Gramotey")
+
+#ifdef WIFI_SSID_CUSTOM
+#define WIFI_SSID WIFI_SSID_CUSTOM
+#else
+#define WIFI_SSID ("SSID")
+#endif
+
+#ifdef WIFI_PASSWORD_CUSTOM
+#define WIFI_PASSWORD WIFI_PASSWORD_CUSTOM
+#else
+#define WIFI_PASSWORD ("PASSWORD")
+#endif
 
 //----------------------------------------------------------------------
 #endif // CONFIG_WIFI_H
